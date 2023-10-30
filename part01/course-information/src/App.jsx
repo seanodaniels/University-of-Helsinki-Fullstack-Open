94d6b3c778c7521ec9418d81f3a537caae5e182c
Sean O'Daniels <sodaniels@gmail.com>
2023-10-30 13:43:29 -0400

    Solution to exercise 1.6: unicafe step1

--- part01/course-information/src/App.jsx
+++ part01/course-information/src/App.jsx
@@ -6,7 +6,6 @@ const Header = (props) => {
   )
 }
 
-
 const Content = (props) => {
   return (
     <>
---
+++ part01/unicafe/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+
+  const [ good, setGood ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+
+  return (
+    <>
+      <h2>give feedback</h2>
+
+      <Button handleClick={()=>setGood(good+1)} text="good" />
+      <Button handleClick={()=>setNeutral(neutral+1)} text="neutral" />
+      <Button handleClick={()=>setBad(bad+1)} text="bad" />
+
+      <h2>statistics</h2>
+
+      <p>
+        good {good}<br />
+        neutral {neutral}<br />
+        bad {bad}
+      </p>
+    </>
+  )
+}
+
+export default App
