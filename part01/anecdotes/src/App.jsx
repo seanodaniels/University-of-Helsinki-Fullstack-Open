fdb219f6759a1f27de6069dfc4eef0edf44aadf0
Sean O'Daniels <sodaniels@gmail.com>
2023-10-31 12:29:56 -0400

    Solution for exercise 1.13: anecdotes step2

--- part01/anecdotes/src/App.jsx
+++ part01/anecdotes/src/App.jsx
@@ -8,19 +8,26 @@ const Button = (props) => {
   )
 }
 
-const App = () => {
+const DisplayAnecdote = (props) => {
+  return (
+    <p>{props.current[0]}<br />
+    has {props.current[1]} votes</p>
+  )
+}
 
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+const App = () => {
   
+  const [ anecdotes, setAnecdotes ] = useState([
+    ['If it hurts, do it more often.', 0],
+    ['Adding manpower to a late software project makes it later!', 0],
+    ['The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', 0],
+    ['Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 0],
+    ['Premature optimization is the root of all evil.', 0],
+    ['Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', 0],
+    ['Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', 0],
+    ['The only way to go fast, is to go well.', 0]
+  ])
+
   const [ selected, setSelected ] = useState(0)
 
   const randomAnecdote = (max) => {
@@ -29,11 +36,26 @@ const App = () => {
     )
   }
 
+  const updateSelected = () => {
+    return(
+      setSelected(randomAnecdote(8))
+    )
+  }
+
+  const updateVote = (props) => {
+    const updatedAnecdotes = [
+      ...anecdotes
+    ]
+    updatedAnecdotes[props][1] += 1
+    setAnecdotes(updatedAnecdotes)
+  }  
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <Button text="next anecdote" handleClick={()=>setSelected(randomAnecdote(8))} />
-    </div>
+      <DisplayAnecdote current={anecdotes[selected]} />
+      <Button text="vote" handleClick={() => updateVote(selected)} />
+      <Button text="next anecdote" handleClick={() => updateSelected()} />
+   </div>
   )
 }
 
