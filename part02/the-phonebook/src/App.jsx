02a01f73e5e27822ebdf2f2c2d20c02932f36786
Sean O'Daniels <sodaniels@gmail.com>
2023-11-03 15:49:52 -0400

    Solution for exercise 2.8: The Phonebook Step3. I also added in error handling for empty fields.

--- part02/the-phonebook/src/App.jsx
+++ part02/the-phonebook/src/App.jsx
@@ -2,26 +2,51 @@ import { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', id: 0 }
+    { name: 'Arto Hellas', number: "040-1234567" }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     const userExists = persons.some(person => 
       person.name === newPerson.name
     )
-    console.log("submitted exists?", userExists)
-    // console.log("test:", newPerson.name)
-    if (!userExists) {
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-    } else {
+
+    var goFlag = true
+
+    // Did the user enter a name?
+    if (!newPerson.name) { 
+      let alertNewName = `${newPerson.name} needs a name`
+      alert(alertNewName)
+      goFlag = false
+    }
+
+    // Did the user enter a number?
+    if (!newPerson.number) { 
+      let alertNewNumber = `${newPerson.name} needs a number`
+      alert(alertNewNumber)
+      goFlag = false
+    }
+
+    // Does the entry already exist?
+    if (userExists) {
       let existsAlert = `${newPerson.name} is already added to phonebook`
       alert(existsAlert)
+      goFlag = false
+    }
+
+    // If no errors then add the entry into the phonebook
+    if (goFlag) {
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+      // Focus on the name field
+      document.getElementById('name-field').focus()
     }
   }
   
@@ -29,19 +54,24 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+        name: <input id="name-field" value={newName} onChange={handleNameChange} /><br />
+        number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}<br /></div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}<br /></div>)}
     </div>
   )
 }
