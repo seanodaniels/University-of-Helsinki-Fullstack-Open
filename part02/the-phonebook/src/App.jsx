07117c6e82f83aa3c48ce0826712959b7de5f709
Sean O'Daniels <sodaniels@gmail.com>
2023-11-04 09:31:27 -0400

    Solution to exercise 2.9: The Phonebook Step4

--- part02/the-phonebook/src/App.jsx
+++ part02/the-phonebook/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: "040-1234567" }
+  // const [ persons, setPersons ] = useState([
+  //   { name: 'Arto Hellas', number: "040-1234567" }
+  // ])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filterName, setFilterName ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -58,9 +65,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleNameFilter = (event) => {
+    setFilterName(event.target.value)
+  }
+  
+  const peopleToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(filterName.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with <input value={filterName} onChange={handleNameFilter} />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
         name: <input id="name-field" value={newName} onChange={handleNameChange} /><br />
@@ -71,7 +90,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}<br /></div>)}
+      {peopleToShow.map(people => 
+        <div key={people.name}>{people.name} {people.number}<br /></div>
+      )}
     </div>
   )
 }
